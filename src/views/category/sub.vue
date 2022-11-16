1c46b5a5c9dee9157ce229e3e5b7212c6c60db43
zsyandrh1314 <1755567510@qq.com>
2022-11-16 13:05:40 +0800

    商品分类-二级分类-结果区数据加载

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -10,6 +10,13 @@
         <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="item in list" :key="item" >
+            <GoodsItem :goods="item" />
+          </li>
+        </ul>
+        <!-- 加载 -->
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData" />
       </div>
     </div>
   </div>
@@ -18,20 +25,61 @@
 <script>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
-import SubSort from './components/sub-sort';
-import { ref } from 'vue'
-  export default {
-    name: 'SubCategory',
-    setup() {
-      const isAllChecked = ref(true)
-      return {isAllChecked}
-    },
-    components: {
-      SubBread,
-      SubFilter,
-      SubSort
+import SubSort from './components/sub-sort'
+import GoodsItem from './components/goods-item'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
+export default {
+  name: 'SubCategory',
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
+  setup () {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
+
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
     }
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        if (result.items.length) {
+          goodsList.value.push(...result.items)
+          reqParams.page++
+        } else {
+          // 加载完毕
+          finished.value = true
+        }
+        // 请求结束
+        loading.value = false
+      })
+    }
+
+    // 切换二级分类重新加载
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && route.path === ('/category/sub/' + newVal)) {
+        goodsList.value = []
+        reqParams = {
+          page: 1,
+          pageSize: 20
+        }
+        finished.value = false
+      }
+    })
+
+    return { loading, finished, goodsList, getData }
   }
+}
 </script>
 
 <style lang="less" scoped>
@@ -39,5 +87,17 @@ import { ref } from 'vue'
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
