e709bce5e37e0ae2bb9736273fc92871e03f0833
zsyandrh1314 <1755567510@qq.com>
2022-11-16 19:38:46 +0800

    商品详情-渲染面包屑

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,13 +1,13 @@
 <template>
 <!-- 商品详情 -->
-  <div class="xtx-goods-page">
+  <div class="xtx-goods-page" v-if="goods">
     <div class="container">
-      <!-- 面包屑 -->
+       <!-- 面包屑 -->
       <XtxBread>
         <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem to="/">手机</XtxBreadItem>
-        <XtxBreadItem to="/">华为</XtxBreadItem>
-        <XtxBreadItem to="/">p30</XtxBreadItem>
+        <XtxBreadItem :to="'/category/'+goods.categories[0].id">{{goods.categories[0].name}}</XtxBreadItem>
+        <XtxBreadItem :to="'/category/sub/'+goods.categories[1].id">{{goods.categories[1].name}}</XtxBreadItem>
+        <XtxBreadItem>{{goods.name}}</XtxBreadItem>
       </XtxBread>
       <!-- 商品信息 -->
       <div class="goods-info"></div>
@@ -30,13 +30,36 @@
 </template>
 
 <script>
-import { GoodsRelevant } from './components/goods-relevant';
-  export default {
-    name: 'Goods',
-    components: {
-      GoodsRelevant
-    }
+import GoodsRelevant from './components/goods-relevant'
+import { nextTick, ref, watch } from 'vue'
+import { findGoods } from '@/api/product'
+import { useRoute } from 'vue-router'
+export default {
+  name: 'XtxGoodsPage',
+  components: { GoodsRelevant },
+  setup () {
+    const goods = useGoods()
+    return { goods }
   }
+}
+// 获取商品详情
+const useGoods = () => {
+  // 出现路由地址商品ID发生变化，但是不会重新初始化组件
+  const goods = ref(null)
+  const route = useRoute()
+  watch(() => route.params.id, (newVal) => {
+    if (newVal && `/product/${newVal}` === route.path) {
+      findGoods(route.params.id).then(data => {
+        // 让商品数据为null让后使用v-if的组件可以重新销毁和创建
+        goods.value = null
+        nextTick(() => {
+          goods.value = data.result
+        })
+      })
+    }
+  }, { immediate: true })
+  return goods
+}
 </script>
 
 <style lang="less" scoped>
