957e16537f0828a7c0523e4c1e6d8b4c3b58edb3
zsyandrh1314 <1755567510@qq.com>
2022-12-02 11:22:21 +0800

    购物车页面-列表展示-本地

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -122,6 +122,27 @@ export default {
     validAmount (state,getters) { // 件数*价格 , *100再/100是防止有浮点数
       // return getters.validList.reduce((p,c) => p + c.nowPrice * 100 * c.count, 0) / 100
       return getters.validList.reduce((p, c) => p + parseInt(c.nowPrice * 100) * c.count, 0) / 100
-    }
+    },
+    // 无效商品列表
+    invalidList (state) {
+      return state.list.filter(goods => goods.stock <= 0 && !goods.isEffective) 
+    },
+    // 已选商品列表 (有效商品里面拿)
+    selectedList (state, getters) { // 从有效商品列表中查已选择的列表
+      return getters.validList.filter(item => item.selected)
+    },
+    // 已选商品总件数
+    selectedTotal(state, getters) {// p 是上一个回调的结果  从0开始累加
+      // reduce 累加
+      return getters.selectedList.reduce((p, c) => p + c.count, 0)
+    },
+    // 已选商品总金额
+    selectedAmount(state, getters) { // 件数*价格 , *100再/100是防止有浮点数 Math.round四舍五入
+      return getters.selectedList.reduce((p, c) => p + Math.round(c.nowPrice * 100) * c.count, 0) / 100
+    },
+    // 是否全选
+    isCheckAll (state, getters) {
+      return getters.validList.length !== 0 && getters.selectedList.length === getters.validList.length
+    } 
   }
 }
